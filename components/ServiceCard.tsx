5383e70c0c3a6fbb50102518b1cb97482a23c1e4
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-08-23 01:53:14 +0300

    Delete components directory

--- components/ServiceCard.tsx
+++
@@ -1,9 +0,0 @@
-type Props = { title: string; desc: string };
-export default function ServiceCard({ title, desc }: Props) {
-  return (
-    <div className="border rounded-2xl p-6 hover:shadow-sm transition">
-      <h3 className="font-bold mb-2">{title}</h3>
-      <p className="text-gray-700">{desc}</p>
-    </div>
-  );
-}
