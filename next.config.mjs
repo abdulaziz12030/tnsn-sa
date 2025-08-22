cf2adc1fa9b09cde652d3b4c7350d648ff981894
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-08-23 01:50:49 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { unoptimized: true, remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] },
-  experimental: { typedRoutes: true }
-};
-export default nextConfig;
